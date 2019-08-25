116f820c4f47722323de8eda3cf609d7a1c7bf4a
karim tarek <karimtarekabdelaziz@gmail.com>
2019-08-25 18:49:29 +0200

    update navbar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,7 +1,55 @@
 <template>
-    <nav class="nav">
-        testing
-    </nav>
+  <nav class="navbar">
+    <!-- START NAVBAR NOTE -->
+    <div class="navbar_note">
+      <!-- START CONTAINER -->
+      <div class="container">
+        <!-- START NAVBAR NOTE CONTENT -->
+        <div class="navbar_note_content">
+          <!-- NAVBAR NOTE PARAGRAPH -->
+          <p
+            class="navbar_note_paragraph text-capitalize"
+          >free international shipping with $75 purchase</p>
+
+          <!-- ICON CLOSE -->
+          <div class="navbar_note_close">
+            <span class="navbar_note_close_icon">&times;</span>
+          </div>
+        </div>
+        <!-- END NAVBAR NOTE CONTENT -->
+      </div>
+      <!-- END CONTAINER -->
+    </div>
+    <!-- END NAVBAR NOTE -->
+
+    <!-- START NAVBAR NAV -->
+    <div class="navbar_primary">
+      <!-- START CONTAINER -->
+      <div class="container">
+        <!-- START NAVBAR PRIMARY BOX -->
+        <div class="navbar_primary_box">
+          <!-- START NAVBAR PRIMARY INFO -->
+          <div class="navbar_primary_info">
+            <!-- NAVBAR PRIMARY INFO TITLE -->
+            <p class="navbar_primary_info_title">hello guys! welcome to me shop</p>
+
+            <!-- NAVBAR PRIMARY INFO EMAIL -->
+            <p class="navbar_primary_info_email">
+              <span class="text-capitalize">email:</span>free@psdfreebies.com
+            </p>
+          </div>
+          <!-- END NAVBAR PRIMARY INFO -->
+
+          <!-- START NAVBAR PRIMARY NAV -->
+          <div class="navbar_primary_nav">test 2</div>
+          <!-- END NAVBAR PRIMARY NAV -->
+        </div>
+        <!-- END NAVBAR PRIMARY BOX -->
+      </div>
+      <!-- END CONTAINER -->
+    </div>
+    <!-- END NAVBAR NAV -->
+  </nav>
 </template>
 
 <script>
