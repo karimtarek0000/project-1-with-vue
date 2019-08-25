f1359fac473691ccd13d2d4e2f804d2ae6631906
karim tarek <karimtarekabdelaziz@gmail.com>
2019-08-25 22:33:34 +0200

    update router js

--- src/router.js
+++ src/router.js
@@ -2,6 +2,11 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
 import Home from './view/Home.vue';
+import Shop from './view/Shop.vue';
+import Product from './view/Product.vue';
+import Blog from './view/Blog.vue';
+import Portfolio from './view/Portfolio.vue';
+import Page from './view/Page.vue';
 
 // PERFORMANCE
 
@@ -17,6 +22,31 @@ export const router = new VueRouter({
             path: '/',
             component: Home,
             name: 'home'
+        },
+        {
+            path: '/shop',
+            component: Shop,
+            name: 'shop'
+        },
+        {
+            path: '/product',
+            component: Product,
+            name: 'product'
+        },
+        {
+            path: '/blog',
+            component: Blog,
+            name: 'blog'
+        },
+        {
+            path: '/portfolio',
+            component: Portfolio,
+            name: 'portfolio'
+        },
+        {
+            path: '/page',
+            component: Page,
+            name: 'page'
         }
     ]
 });
