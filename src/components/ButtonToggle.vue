efdd80959c5c9841aa0d00d32b5881cae57882b5
karim tarek <karimtarekabdelaziz@gmail.com>
2019-08-30 01:39:40 +0200

    update button toggle vue

--- src/components/ButtonToggle.vue
+++ src/components/ButtonToggle.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="navbar_primary_nav_toggle" ref="toggleNavbar">
+  <div class="navbar_primary_nav_toggle" ref="toggleNavbar" @click="runningAnimation">
     <span class="navbar_primary_nav_toggle_icon navbar_primary_nav_toggle_icon--1"></span>
     <span class="navbar_primary_nav_toggle_icon navbar_primary_nav_toggle_icon--2"></span>
     <span class="navbar_primary_nav_toggle_icon navbar_primary_nav_toggle_icon--3"></span>
@@ -8,70 +8,62 @@
 
 <script>
 export default {
-  mounted() {
-    const btnToggle = () => {
-      const parentBtnToggle = this.$refs.toggleNavbar,
+    data() {
+        return {
+            tl: new TimelineMax()
+        }
+    },
+  methods: {
+    // RUNNING ANIMATION
+    runningAnimation() {
+
+        // PARENT BTN TOGGLE
+        const parentBtnToggle = this.$refs.toggleNavbar;
+
+      // TIME LINE TRACK
+      if(!parentBtnToggle.classList.contains('active')) {
+        
+        // ADD CLASS ACTIVE
+        parentBtnToggle.classList.add('active');
+        this.trackAnimation(false);
+
+      } else {
+
+        // REMOVE CLASS ACTIVE
+        parentBtnToggle.classList.remove('active');
+        const timeLineReverse = this.tl.reversed();
+
+        // PATH TRUE TL.REVERSED
+        this.trackAnimation(true);
+
+        // TOGGLE PLAY BACK
+        this.tl.reversed(`!${timeLineReverse}`);
+      }
+
+        
+    },
+    
+    // TRACK ANIMATION
+    trackAnimation(status) {
+        // ALL VARIABLES
+        const parentBtnToggle = this.$refs.toggleNavbar,
         childsBtnToggleOne = this.$refs.toggleNavbar.children[0],
         childsBtnToggleTwo = this.$refs.toggleNavbar.children[1],
         childsBtnToggleThree = this.$refs.toggleNavbar.children[2],
         navbarItems = document.getElementById("navbar-primary"),
         duration = ".5";
 
-      const tl = new TimelineMax();
-
-      // TIME LINE TRACK
-      tl.to(childsBtnToggleOne, duration, {
-        position: "relative",
-        transform: "translateY(9px) rotate(45deg)",
-        ease: Power2.ease
-      })
-        .to(
-          childsBtnToggleTwo,
-          0.1,
-          { autoAlpha: 0, ease: Power2.ease },
-          "=-.5"
-        )
-        .to(
-          childsBtnToggleThree,
-          duration,
-          {
-            position: "relative",
-            transform: "translateY(-6px) rotate(-45deg)",
-            ease: Power2.ease
-          },
-          "=-.5"
-        )
-        .to(navbarItems, duration, {
-          autoAlpha: 1,
-          right: "0",
-          ease: Power1.easeInOut
-        });
-
-      // TIME LINE PAUSE
-      tl.pause();
-
-      // ADD EVENT LISTENER
-      parentBtnToggle.addEventListener("click", () => {
-        if (!parentBtnToggle.classList.contains("active")) {
-          // ADD CLASS ACTIVE
-          parentBtnToggle.classList.add("active");
-
-          // STATUS TIME LINE ANIMATION
-          tl.play();
-          tl.reversed(true);
-        } else {
-          // REMOVE CLASS ACTIVE
-          parentBtnToggle.classList.remove("active");
-
-          // IF TL REVERSED WILL BE PLAY BACK
-          tl.reversed(!tl.reversed());
-        }
-      });
-    };
-
-    // RUNNING FUNCTION
-    btnToggle();
-  }
+        // TIME LINE TRACK
+        this.tl
+            .to(childsBtnToggleOne, duration, {position: "relative",transform: "translateY(9px) rotate(45deg)",ease: Power2.ease})
+            .to(childsBtnToggleTwo,0.1,{ autoAlpha: 0, ease: Power2.ease },"=-.5")
+            .to(childsBtnToggleThree,duration,{position: "relative",transform: "translateY(-6px) rotate(-45deg)",ease: Power2.ease},"=-.5")
+            .to(navbarItems, duration, {autoAlpha: 1,right: "0",ease: Power1.easeInOut});
+        
+        // TL REVERSED
+        this.tl.reversed(status);
+    }
+  },
 };
 </script>
 
