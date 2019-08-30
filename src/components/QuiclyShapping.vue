365b5b9799812ae2937946987c6b1cd850737e95
karim tarek <karimtarekabdelaziz@gmail.com>
2019-08-30 19:46:13 +0200

    add new component into components vue

---
+++ src/components/QuiclyShapping.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="featured_products_quicly_shapping text-capitalize">
+        <!-- START QUICLY SHAPPING BOXS -->
+        <div class="featured_products_quicly_shapping_boxs">
+
+            <div class="featured_products_quicly_shapping_box">
+                <img src="../assets/img/shopping-1/1.png">
+                <div class="featured_products_quicly_shapping_box_content">
+                    <h3 class="featured_products_quicly_shapping_box_content_heading">
+                        <span>fashion</span>
+                        <span>camival</span>
+                    </h3>
+                    <p class="featured_products_quicly_shapping_box_content_par">Lorem ipsum, dolor sit amet consectetur adipisicing elit</p>
+                    <a class="featured_products_quicly_shapping_box_content_btn btn-primary reset-link" href="#">shop now</a>
+                </div>
+            </div>
+
+            <div class="featured_products_quicly_shapping_box">
+                <img src="../assets/img/shopping-1/2.png">
+                <div class="featured_products_quicly_shapping_box_content">
+                    <h3 class="featured_products_quicly_shapping_box_content_heading">
+                        <span>fashion</span>
+                        <span>camival</span>
+                    </h3>
+                    <p class="featured_products_quicly_shapping_box_content_par">Lorem ipsum, dolor sit amet consectetur adipisicing elit</p>
+                    <a class="featured_products_quicly_shapping_box_content_btn btn-primary reset-link" href="#">shop now</a>
+                </div>
+            </div>
+
+        </div>
+    </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
