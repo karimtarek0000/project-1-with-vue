f5ff7c145764d0cf8e6aee1e57cd8f2ea66bf9c9
karim tarek <karimtarekabdelaziz@gmail.com>
2019-08-30 19:51:32 +0200

    update home vue

--- src/view/Home.vue
+++ src/view/Home.vue
@@ -1,15 +1,22 @@
 <template>
+  <div>
+    <!-- HEADER -->
     <app-header></app-header>
+
+    <!-- FIRST SECTION -->
+    <app-featured-products></app-featured-products>
+  </div>
 </template>
 
 <script>
-
 // ALL IMPORT
-import Header from '../components/Header.vue';
+import Header from "../components/Header.vue";
+import FeaturedProducts from "../components/FeaturedProducts";
 
 export default {
   components: {
     appHeader: Header,
+    appFeaturedProducts: FeaturedProducts
   }
 };
 </script>
