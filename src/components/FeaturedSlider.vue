17c692140ecf0448216217c691797a7b48592454
karim tarek <karimtarekabdelaziz@gmail.com>
2019-08-31 03:03:47 +0200

    add new component featured slider

---
+++ src/components/FeaturedSlider.vue
@@ -0,0 +1,296 @@
+<template>
+  <div class="featured_products_slider">
+    <div class="featured_products_slider_heading text-center text-capitalize">
+      <h3 class="featured_products_slider_heading_head">featured products</h3>
+      <p
+        class="featured_products_slider_heading_par"
+      >praesent mauris. fusce nec tellus sed augue semper porta.</p>
+    </div>
+
+    <!-- START FEATURED PRODUCTS SLIDER INNER -->
+    <div class="featured_products_slider_inner">
+
+      <div class="featured_products_slider_slide">
+        <div class="featured_products_slider_slide_image">
+          <img src="../assets/img/slider/1.png" alt="image-one" />
+        </div>
+
+        <div class="featured_products_slider_slide_caption">
+          <h4 class="eatured_products_slider_slide_head">orange t-shirt</h4>
+          <ul class="featured_products_slider_slide_rating list-unstyled">
+            <li>
+              <svg>
+                <use xlink:href="../assets/img/sprit.svg#icon-star-o" />
+              </svg>
+            </li>
+            <li>
+              <svg>
+                <use xlink:href="../assets/img/sprit.svg#icon-star-o" />
+              </svg>
+            </li>
+            <li>
+              <svg>
+                <use xlink:href="../assets/img/sprit.svg#icon-star-o" />
+              </svg>
+            </li>
+            <li>
+              <svg>
+                <use xlink:href="../assets/img/sprit.svg#icon-star-o" />
+              </svg>
+            </li>
+            <li>
+              <svg>
+                <use xlink:href="../assets/img/sprit.svg#icon-star-o" />
+              </svg>
+            </li>
+          </ul>
+          <span class="eatured_products_slider_slide_price">$500</span>
+          <span class="eatured_products_slider_slide_discount">$600</span>
+        </div>
+      </div>
+
+      <div class="featured_products_slider_slide">
+        <div class="featured_products_slider_slide_image">
+          <img src="../assets/img/slider/2.png" alt="image-two" />
+        </div>
+
+        <div class="featured_products_slider_slide_caption">
+          <h4 class="eatured_products_slider_slide_head">orange t-shirt</h4>
+          <ul class="featured_products_slider_slide_rating list-unstyled">
+            <li>
+              <svg>
+                <use xlink:href="../assets/img/sprit.svg#icon-star-o" />
+              </svg>
+            </li>
+            <li>
+              <svg>
+                <use xlink:href="../assets/img/sprit.svg#icon-star-o" />
+              </svg>
+            </li>
+            <li>
+              <svg>
+                <use xlink:href="../assets/img/sprit.svg#icon-star-o" />
+              </svg>
+            </li>
+            <li>
+              <svg>
+                <use xlink:href="../assets/img/sprit.svg#icon-star-o" />
+              </svg>
+            </li>
+            <li>
+              <svg>
+                <use xlink:href="../assets/img/sprit.svg#icon-star-o" />
+              </svg>
+            </li>
+          </ul>
+          <span class="eatured_products_slider_slide_price">$500</span>
+          <span class="eatured_products_slider_slide_discount">$600</span>
+        </div>
+      </div>
+
+      <div class="featured_products_slider_slide">
+        <div class="featured_products_slider_slide_image">
+          <img src="../assets/img/slider/3.png" alt="image-three" />
+        </div>
+
+        <div class="featured_products_slider_slide_caption">
+          <h4 class="eatured_products_slider_slide_head">orange t-shirt</h4>
+          <ul class="featured_products_slider_slide_rating list-unstyled">
+            <li>
+              <svg>
+                <use xlink:href="../assets/img/sprit.svg#icon-star-o" />
+              </svg>
+            </li>
+            <li>
+              <svg>
+                <use xlink:href="../assets/img/sprit.svg#icon-star-o" />
+              </svg>
+            </li>
+            <li>
+              <svg>
+                <use xlink:href="../assets/img/sprit.svg#icon-star-o" />
+              </svg>
+            </li>
+            <li>
+              <svg>
+                <use xlink:href="../assets/img/sprit.svg#icon-star-o" />
+              </svg>
+            </li>
+            <li>
+              <svg>
+                <use xlink:href="../assets/img/sprit.svg#icon-star-o" />
+              </svg>
+            </li>
+          </ul>
+          <span class="eatured_products_slider_slide_price">$500</span>
+          <span class="eatured_products_slider_slide_discount">$600</span>
+        </div>
+      </div>
+
+       <div class="featured_products_slider_slide">
+        <div class="featured_products_slider_slide_image">
+          <img src="../assets/img/slider/4.png" alt="image-four" />
+        </div>
+
+        <div class="featured_products_slider_slide_caption">
+          <h4 class="eatured_products_slider_slide_head">orange t-shirt</h4>
+          <ul class="featured_products_slider_slide_rating list-unstyled">
+            <li>
+              <svg>
+                <use xlink:href="../assets/img/sprit.svg#icon-star-o" />
+              </svg>
+            </li>
+            <li>
+              <svg>
+                <use xlink:href="../assets/img/sprit.svg#icon-star-o" />
+              </svg>
+            </li>
+            <li>
+              <svg>
+                <use xlink:href="../assets/img/sprit.svg#icon-star-o" />
+              </svg>
+            </li>
+            <li>
+              <svg>
+                <use xlink:href="../assets/img/sprit.svg#icon-star-o" />
+              </svg>
+            </li>
+            <li>
+              <svg>
+                <use xlink:href="../assets/img/sprit.svg#icon-star-o" />
+              </svg>
+            </li>
+          </ul>
+          <span class="eatured_products_slider_slide_price">$500</span>
+          <span class="eatured_products_slider_slide_discount">$600</span>
+        </div>
+      </div>
+
+      <div class="featured_products_slider_slide">
+        <div class="featured_products_slider_slide_image">
+          <img src="../assets/img/slider/4.png" alt="image-four" />
+        </div>
+
+        <div class="featured_products_slider_slide_caption">
+          <h4 class="eatured_products_slider_slide_head">orange t-shirt</h4>
+          <ul class="featured_products_slider_slide_rating list-unstyled">
+            <li>
+              <svg>
+                <use xlink:href="../assets/img/sprit.svg#icon-star-o" />
+              </svg>
+            </li>
+            <li>
+              <svg>
+                <use xlink:href="../assets/img/sprit.svg#icon-star-o" />
+              </svg>
+            </li>
+            <li>
+              <svg>
+                <use xlink:href="../assets/img/sprit.svg#icon-star-o" />
+              </svg>
+            </li>
+            <li>
+              <svg>
+                <use xlink:href="../assets/img/sprit.svg#icon-star-o" />
+              </svg>
+            </li>
+            <li>
+              <svg>
+                <use xlink:href="../assets/img/sprit.svg#icon-star-o" />
+              </svg>
+            </li>
+          </ul>
+          <span class="eatured_products_slider_slide_price">$500</span>
+          <span class="eatured_products_slider_slide_discount">$600</span>
+        </div>
+      </div>
+
+       <div class="featured_products_slider_slide">
+        <div class="featured_products_slider_slide_image">
+          <img src="../assets/img/slider/2.png" alt="image-two" />
+        </div>
+
+        <div class="featured_products_slider_slide_caption">
+          <h4 class="eatured_products_slider_slide_head">orange t-shirt</h4>
+          <ul class="featured_products_slider_slide_rating list-unstyled">
+            <li>
+              <svg>
+                <use xlink:href="../assets/img/sprit.svg#icon-star-o" />
+              </svg>
+            </li>
+            <li>
+              <svg>
+                <use xlink:href="../assets/img/sprit.svg#icon-star-o" />
+              </svg>
+            </li>
+            <li>
+              <svg>
+                <use xlink:href="../assets/img/sprit.svg#icon-star-o" />
+              </svg>
+            </li>
+            <li>
+              <svg>
+                <use xlink:href="../assets/img/sprit.svg#icon-star-o" />
+              </svg>
+            </li>
+            <li>
+              <svg>
+                <use xlink:href="../assets/img/sprit.svg#icon-star-o" />
+              </svg>
+            </li>
+          </ul>
+          <span class="eatured_products_slider_slide_price">$500</span>
+          <span class="eatured_products_slider_slide_discount">$600</span>
+        </div>
+      </div>
+
+      <div class="featured_products_slider_slide">
+        <div class="featured_products_slider_slide_image">
+          <img src="../assets/img/slider/1.png" alt="image-one" />
+        </div>
+
+        <div class="featured_products_slider_slide_caption">
+          <h4 class="eatured_products_slider_slide_head">orange t-shirt</h4>
+          <ul class="featured_products_slider_slide_rating list-unstyled">
+            <li>
+              <svg>
+                <use xlink:href="../assets/img/sprit.svg#icon-star-o" />
+              </svg>
+            </li>
+            <li>
+              <svg>
+                <use xlink:href="../assets/img/sprit.svg#icon-star-o" />
+              </svg>
+            </li>
+            <li>
+              <svg>
+                <use xlink:href="../assets/img/sprit.svg#icon-star-o" />
+              </svg>
+            </li>
+            <li>
+              <svg>
+                <use xlink:href="../assets/img/sprit.svg#icon-star-o" />
+              </svg>
+            </li>
+            <li>
+              <svg>
+                <use xlink:href="../assets/img/sprit.svg#icon-star-o" />
+              </svg>
+            </li>
+          </ul>
+          <span class="eatured_products_slider_slide_price">$500</span>
+          <span class="eatured_products_slider_slide_discount">$600</span>
+        </div>
+      </div>
+
+    </div>
+    <!-- END FEATURED PRODUCTS SLIDER INNER -->
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
