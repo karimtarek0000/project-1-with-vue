bfbd42011a8dc765dee9c0b345b779d5c6f54984
karim tarek <karimtarekabdelaziz@gmail.com>
2019-09-03 18:51:19 +0200

    update home vue

--- src/view/Home.vue
+++ src/view/Home.vue
@@ -5,6 +5,10 @@
 
     <!-- FIRST SECTION -->
     <app-featured-products></app-featured-products>
+
+    <!-- LATEST NEWS -->
+    <app-latest-news></app-latest-news>
+    
   </div>
 </template>
 
@@ -12,11 +16,13 @@
 // ALL IMPORT
 import Header from "../components/Header.vue";
 import FeaturedProducts from "../components/FeaturedProducts";
+import LatestNews from "../components/LatestNews";
 
 export default {
   components: {
     appHeader: Header,
-    appFeaturedProducts: FeaturedProducts
+    appFeaturedProducts: FeaturedProducts,
+    appLatestNews: LatestNews
   }
 };
 </script>
