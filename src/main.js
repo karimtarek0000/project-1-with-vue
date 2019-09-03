fbd45284f800df99f187fb2514802d15b31d25dc
karim tarek <karimtarekabdelaziz@gmail.com>
2019-09-03 18:50:55 +0200

    update main js

--- src/main.js
+++ src/main.js
@@ -2,9 +2,13 @@
 import 'sass/style';
 import Vue from 'vue';
 import App from './App';
+import VueResource from 'vue-resource';
 import { router } from './router';
 import { store } from './store/store';
 import { TweenMax, TimelineMax } from 'greensock';
+
+//
+Vue.use(VueResource);
 
 // RENDER
 new Vue({
@@ -12,4 +16,3 @@ new Vue({
     store,
     render: h => h(App)
 }).$mount('#app');
-
