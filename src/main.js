29334ef9af6126848234ad2771ec0c2ae9536e55
karim tarek <karimtarekabdelaziz@gmail.com>
2019-09-05 12:09:56 +0200

    update main js

--- src/main.js
+++ src/main.js
@@ -7,7 +7,7 @@ import { router } from './router';
 import { store } from './store/store';
 import { TweenMax, TimelineMax } from 'greensock';
 
-//
+// USE VUE RESOURCE
 Vue.use(VueResource);
 
 // RENDER
