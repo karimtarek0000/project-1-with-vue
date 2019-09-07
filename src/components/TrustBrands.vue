057dae56a832c94cc40aef578ad8c4efe2628d8b
karim tarek <karimtarekabdelaziz@gmail.com>
2019-09-07 17:01:22 +0200

    add new component vue

---
+++ src/components/TrustBrands.vue
@@ -0,0 +1,59 @@
+<template>
+    <section class="trust_brands">
+
+        <!-- START CONTAINER -->
+        <div class="container">
+
+            <!-- START TRUST BRANDS BOX -->
+            <div class="trust_brands_box text-center">
+
+                <div>
+                    <svg>
+                        <use xlink:href="../assets/img/sprit.svg#icon-logo-1"></use>
+                    </svg>
+                </div>
+
+                <div>
+                    <svg>
+                        <use xlink:href="../assets/img/sprit.svg#icon-logo-2"></use>
+                    </svg>
+                </div>
+
+                <div>
+                    <svg>
+                        <use xlink:href="../assets/img/sprit.svg#icon-logo-3"></use>
+                    </svg>
+                </div>
+
+                <div>
+                    <svg>
+                        <use xlink:href="../assets/img/sprit.svg#icon-logo-4"></use>
+                    </svg>
+                </div>
+
+                <div>
+                    <svg>
+                        <use xlink:href="../assets/img/sprit.svg#icon-logo-5"></use>
+                    </svg>
+                </div>
+
+                <div>
+                    <svg>
+                        <use xlink:href="../assets/img/sprit.svg#icon-logo-1"></use>
+                    </svg>
+                </div>
+
+            </div>
+            <!-- END TRUST BRANDS BOX -->
+
+        </div>
+        <!-- END CONTAINER -->
+    </section>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
