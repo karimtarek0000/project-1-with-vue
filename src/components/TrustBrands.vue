0631510abc92a9cb73bbcdf17997f62a445dc153
karim tarek <karimtarekabdelaziz@gmail.com>
2019-09-07 17:39:49 +0200

    update trust brands vue

--- src/components/TrustBrands.vue
+++ src/components/TrustBrands.vue
@@ -5,7 +5,7 @@
         <div class="container">
 
             <!-- START TRUST BRANDS BOX -->
-            <div class="trust_brands_box text-center">
+            <div id="brands" class="trust_brands_box text-center">
 
                 <div>
                     <svg>
@@ -52,7 +52,66 @@
 </template>
 
 <script>
-export default {};
+import { setTimeout } from 'timers';
+export default {
+    data() {
+        return {
+            count: 0,
+            durationTimeOut: 2000,
+            durationSetInterVal: 4000
+
+        }
+    },
+    methods: {
+
+        // BRANDS
+        brands() {
+
+            // ALL VARIABLES
+            const parentBrands = document.getElementById('brands').children;
+            const countParentChild = parentBrands.length-1 
+            
+            // IF STATEMENT WINDOW WIDTH GT 900
+            if(window.innerWidth > 900) {
+
+                // ALL ACTION WILL BE RUNNING SETINTERVAL
+                setInterval(() => {
+                    
+                    // IF STATEMENT COUNT 
+                    if(this.count > countParentChild) {
+                        this.count = 0;
+                    } else if (this.count < 0) {
+                        this.count = countParentChild;
+                    }
+    
+                    // CHNAGE OPACITY CHILD BRANDS
+                    parentBrands[this.count].style.opacity = 1;
+    
+    
+                    // SET TIME OUT WILL BE CHANGE OPACITY AND INCREASE COUNT
+                    setTimeout( () => {
+                        
+                        // OPACITY CHILD BRANDS
+                        parentBrands[this.count].style.opacity = .5;
+    
+                        // INCREASE COUNT
+                        this.count++;
+    
+                    }, this.durationTimeOut);
+    
+    
+                }, this.durationSetInterVal);
+
+            }
+        }
+        
+    },
+    mounted() {
+
+        // RUNNING BRANDS
+        this.brands();
+    }
+};
 </script>
 
 <style>
